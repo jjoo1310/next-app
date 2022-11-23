f7bab346cf5d2ab7f73d7f28edcc27a96f9e96a6
jjoo1310 <jjoo1310@naver.com>
2022-11-23 19:55:50 +0900

    commit

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -20,7 +20,7 @@ function Banner(props: Props) {
 
     return (
         <div className="flex flex-col py-16 space-y-2 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-            <div className='absolute top-0 left-0 -z-10 h-[95vh] w-screen'>
+            <div className='absolute top-0 left-0 -z-10 h-[95vh] w-full'>
                 <Image
                     src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
                     alt=""
