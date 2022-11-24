92795a2843665a359439c8ca674b2a395ce81105
jjoo1310 <jjoo1310@naver.com>
2022-11-24 11:41:41 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Row from '../components/Row'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -20,7 +21,7 @@ const Home = (props: Props) => {
   const { netflixOriginals, trendingNow, topRated, actionMovies, comedyMovies, horrorMovies, romanceMovies, documentaries } = props;
 
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+    <div className='relative h-screen bg-gradient-to-b lg:h-[140vh]'>
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -28,13 +29,18 @@ const Home = (props: Props) => {
       <Header />
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+        <section className='md:space-y-24'>
+          <Row title='Trending Now' movies={trendingNow} />
+          <Row title='Top Rated' movies={topRated} />
+          <Row title='Action Thrillers' movies={actionMovies} />
+          {/* MyList Component */}
+          {
+            // list.length > 0 && <Row title='My List' movies={list} />
+          }
+          <Row title='Comedies' movies={comedyMovies} />
+          <Row title='Scary Movies' movies={horrorMovies} />
+          <Row title='Romance Movies' movies={romanceMovies} />
+          <Row title='Documentaries' movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
